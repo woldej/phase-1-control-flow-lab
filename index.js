819ab4c2459d1ef4bb279885b97507029138319f
woldejamawolde <wolde.jama@m0ringaschool.com>
2023-10-04 23:13:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+  if(ride >2500 ){
+    return 'No can do.'}
+  if (ride <= 400){
+    return 'This one is on me!' }
+  else if(ride >= 2000){
+    return 'I will gladly take your thirty bucks.'}
+   if(ride >2500 ){
+    return 'No can do.'}
+  
+  
+  }
+  
+  function ternaryCheckCity(cityName){
+    return (cityName=="NYC")?'Ok, sounds good.':'No go.';
+    
+    // Write your code here!
+  }
+  function switchOnCharmFromTip(tip){
+  switch(tip){
+  case 'generous':{
+    return 'Thank you so much.'
+    break;
+  }
+  case 'not as generous':
+  {
+    return 'Thank you.'
+    break;
+  }
+  case 'thanks for everything':
+  {
+    return 'Bye.'
+    break;
+  
+  }
+  }
+  }
